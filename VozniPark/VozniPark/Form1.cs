c6d440ce6aee3f7822acb0bb37218715782fe527
84k1 <milos2@teol.net>
2018-12-11 16:51:20 +0100

    Pocetna forma, zapocet template

--- VozniPark/VozniPark/Form1.cs
+++ VozniPark/VozniPark/Form1.cs
@@ -16,5 +16,10 @@ namespace VozniPark
         {
             InitializeComponent();
         }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
--- VozniPark/VozniPark/PropertiesClass/PropertyClassServisiranjeVozila.cs
+++ VozniPark/VozniPark/PropertiesClass/PropertyClassServisiranjeVozila.cs
@@ -1,5 +1,4 @@
-﻿using KonekcijaNaBazu.AttributesClass;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data.SqlClient;
