b94285662a002b2bc53811eb15043ff476bc6da4
Dejan Babic <b.dejan96@outlook.com>
2018-12-11 13:38:32 +0100

    Dodan folder AttributesClass. I 3 atribut klase

---
+++ VozniPark/VozniPark/AttributesClass/ForeignKeyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VozniPark.AttributesClass
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class ForeignKeyAttribute : Attribute
+    {
+        public string referencedTable;
+        public string referencedColumn;
+
+        public ForeignKeyAttribute(string refTable, string refColumn)
+        {
+            this.referencedTable = refTable;
+            this.referencedColumn = refColumn;
+
+        }
+    }
+}
---
+++ VozniPark/VozniPark/AttributesClass/PrimaryKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VozniPark.AttributesClass
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class PrimaryKeyAttribute : Attribute
+    {
+
+    }
+}
